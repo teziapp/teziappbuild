aa4b67e494d5be73e705f7b13e1e7d10ffb29e04
Balkrishna Agarwal <agarwalbalkrishnaindia@gmail.com>
2021-03-16 20:46:14 +0530

    deleting all files from build

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-export const COUCH_URL = 'http://localhost:5984';
-
-export const COUCH_DB_MASTERS = 'masters';
-export const COUCH_DB_USERS= '_users';
-export const COUCH_DB_ORDERS= 'orders';
-
-export const COUCH_USER = 'admin';
-export const COUCH_PASSWORD = 'secret';
